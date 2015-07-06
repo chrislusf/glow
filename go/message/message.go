c2239939977522252aeae2652ac8e7ea5b6f9d29
chrislusf <chris.lu@gmail.com>
2015-07-06 00:23:25 -0700

    Add message Serial/Deserial

--- go/message/message.go
+++ go/message/message.go
@@ -1,7 +1,13 @@
 // message.go
 package message
 
-import ()
+import (
+	"fmt"
+	"io"
+
+	"github.com/chrislusf/glow/go/util"
+	"github.com/golang/protobuf/proto"
+)
 
 type MessageType uint8
 
@@ -16,4 +22,52 @@ type Message struct {
 	Type           MessageType
 	ControlMessage ControlMessage
 	Data           []byte
+}
+
+func (m *Message) Write(w io.Writer) (err error) {
+	buf := make([]byte, 5)
+	buf[0] = byte(m.Type)
+	var data []byte
+	if m.Type == ControlType {
+		data, err = proto.Marshal(&m.ControlMessage)
+		if err != nil {
+			return fmt.Errorf("Write error:%v", err)
+		}
+	} else {
+		data = m.Data
+	}
+	util.Uint32toBytes(buf[1:], uint32(len(data)))
+	w.Write(buf)
+	w.Write(data)
+	return nil
+}
+
+func Read(r io.Reader) (m *Message, err error) {
+	buf := make([]byte, 5)
+	count, err := r.Read(buf)
+	if count == 0 || err != nil {
+		return nil, err
+	}
+	mt := MessageType(buf[0])
+	size := int(util.BytesToUint32(buf[1:]))
+	data := make([]byte, size)
+	count, err = r.Read(data)
+	if err != nil {
+		return nil, err
+	}
+	if count != size {
+		return nil, fmt.Errorf("Wrong message size: expected %d, actual %d", size, count)
+	}
+	m = &Message{
+		Type: mt,
+	}
+	if mt == ControlType {
+		err = proto.Unmarshal(data, &m.ControlMessage)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		m.Data = data
+	}
+	return nil, nil
 }
