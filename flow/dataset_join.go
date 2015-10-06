2e826efbfb671ad42a85a4087630a40d7adbf18d
chrislusf <chris.lu@gmail.com>
2015-10-06 00:10:47 -0700

    simplified join

--- flow/dataset_join.go
+++ flow/dataset_join.go
@@ -45,13 +45,15 @@ func getComparator(dt reflect.Type) (funcPointer interface{}) {
 
 // assume nothing about these two dataset
 func (d *Dataset) Join(other *Dataset) *Dataset {
-	d = d.Partition(len(d.Shards)).LocalSort(nil)
-	other = other.Partition(len(d.Shards)).LocalSort(nil)
-	return d.JoinHashedSorted(other, nil, false, false)
+	sorted_d := d.Partition(len(d.Shards)).LocalSort(nil)
+	if d == other {
+		return sorted_d.SelfJoin(nil)
+	}
+	sorted_other := other.Partition(len(d.Shards)).LocalSort(nil)
+	return sorted_d.JoinHashedSorted(sorted_other, nil, false, false)
 }
 
 // Join multiple datasets that are sharded by the same key, and locally sorted within the shard
-// "this" dataset is the driving dataset and should have more data than "that"
 func (this *Dataset) JoinHashedSorted(that *Dataset,
 	compareFunc interface{}, isLeftOuterJoin, isRightOuterJoin bool,
 ) (ret *Dataset) {
@@ -68,11 +70,15 @@ func (this *Dataset) JoinHashedSorted(that *Dataset,
 		rightChan := task.Inputs[1].ReadChan
 
 		// get first value from both channels
-		rightKey, rightValue, rightHasValue := getKeyValue(rightChan)
 		leftKey, leftValue, leftHasValue := getKeyValue(leftChan)
+		rightKey, rightValue, rightHasValue := getKeyValue(rightChan)
 
 		if compareFunc == nil {
-			compareFunc = getComparator(reflect.TypeOf(leftKey))
+			if leftHasValue {
+				compareFunc = getComparator(reflect.TypeOf(leftKey))
+			} else if rightHasValue {
+				compareFunc = getComparator(reflect.TypeOf(rightKey))
+			}
 		}
 		fn := reflect.ValueOf(compareFunc)
 		comparator := func(a, b interface{}) int64 {
@@ -83,64 +89,71 @@ func (this *Dataset) JoinHashedSorted(that *Dataset,
 			return outs[0].Int()
 		}
 
-		for leftHasValue {
-			if !rightHasValue {
-				if isLeftOuterJoin {
-					// outChan.Send(reflect.ValueOf(NewTuple3(leftKey, leftValue, nil)))
-					send(outChan, leftKey, leftValue, nil)
-				}
-				leftKey, leftValue, leftHasValue = getKeyValue(leftChan)
-				continue
-			}
-
+		var leftValues, rightValues []interface{}
+		for leftHasValue && rightHasValue {
 			x := comparator(leftKey, rightKey)
 			switch {
 			case x == 0:
-				// collect all values on rightChan
-				rightValues := []interface{}{rightValue}
-				prevRightKey := rightKey
-				for {
-					rightKey, rightValue, rightHasValue = getKeyValue(rightChan)
-					if rightHasValue && comparator(prevRightKey, rightKey) == 0 {
-						rightValues = append(rightValues, rightValue)
-						continue
-					} else {
-						// for current left key, join with all rightValues
-						for _, rv := range rightValues {
-							// outChan.Send(reflect.ValueOf(NewTuple3(leftKey, leftValue, rv)))
-							send(outChan, leftKey, leftValue, rv)
-						}
-						// reset right loop
-						rightValues = rightValues[0:0]
-						leftKey, leftValue, leftHasValue = getKeyValue(leftChan)
-						break
+				leftKey, leftValue, leftValues, leftHasValue = getSameKeyValues(leftChan, comparator, leftKey, leftValue)
+				rightKey, rightValue, rightValues, rightHasValue = getSameKeyValues(rightChan, comparator, rightKey, rightValue)
+
+				// left and right cartician join
+				for _, a := range leftValues {
+					for _, b := range rightValues {
+						send(outChan, leftKey, a, b)
 					}
 				}
 			case x < 0:
 				if isLeftOuterJoin {
-					// outChan.Send(reflect.ValueOf(NewTuple3(leftKey, leftValue, nil)))
 					send(outChan, leftKey, leftValue, nil)
 				}
 				leftKey, leftValue, leftHasValue = getKeyValue(leftChan)
 			case x > 0:
 				if isRightOuterJoin {
-					// outChan.Send(reflect.ValueOf(NewTuple3(rightKey, nil, rightValue)))
 					send(outChan, rightKey, nil, rightValue)
 				}
 				rightKey, rightValue, rightHasValue = getKeyValue(rightChan)
 			}
 		}
-		// handle right outer join tail case
+		if leftHasValue {
+			if isLeftOuterJoin {
+				send(outChan, leftKey, leftValue, nil)
+			}
+		}
+		for leftKeyValue := range leftChan {
+			if isLeftOuterJoin {
+				leftKey, leftValue = leftKeyValue.Field(0), leftKeyValue.Field(1)
+				send(outChan, leftKey, leftValue, nil)
+			}
+		}
+		if rightHasValue {
+			if isRightOuterJoin {
+				send(outChan, rightKey, nil, rightValue)
+			}
+		}
 		for rightKeyValue := range rightChan {
 			if isRightOuterJoin {
-				rightKey, rightValue := rightKeyValue.Field(0), rightKeyValue.Field(1)
-				// outChan.Send(reflect.ValueOf(NewTuple3(rightKey, nil, rightValue)))
+				rightKey, rightValue = rightKeyValue.Field(0), rightKeyValue.Field(1)
 				send(outChan, rightKey, nil, rightValue)
 			}
 		}
 
 	}
 	return ret
+}
+
+func getSameKeyValues(ch chan reflect.Value, comparator func(a, b interface{}) int64, theKey, firstValue interface{}) (nextKey, nextValue interface{}, theValues []interface{}, hasValue bool) {
+	theValues = append(theValues, firstValue)
+	for {
+		nextKey, nextValue, hasValue = getKeyValue(ch)
+		if hasValue && comparator(theKey, nextKey) == 0 {
+			theValues = append(theValues, nextValue)
+		} else {
+			return
+		}
+
+	}
+	return
 }
 
 func getKeyValue(ch chan reflect.Value) (key, value interface{}, ok bool) {
@@ -154,4 +167,51 @@ func getKeyValue(ch chan reflect.Value) (key, value interface{}, ok bool) {
 
 func send(outChan reflect.Value, values ...interface{}) {
 	outChan.Send(reflect.ValueOf(values))
+}
+
+func (d *Dataset) SelfJoin(compareFunc interface{}) (ret *Dataset) {
+	outType := reflect.TypeOf([]interface{}{})
+	ret, step := add1ShardTo1Step(d, outType)
+	step.Name = "SelfJoin"
+	step.Function = func(task *Task) {
+		outChan := task.Outputs[0].WriteChan
+
+		leftChan := task.Inputs[0].ReadChan
+
+		// get first value from both channels
+		leftKey, leftValue, leftHasValue := getKeyValue(leftChan)
+
+		// get comparator
+		if compareFunc == nil {
+			if leftHasValue {
+				compareFunc = getComparator(reflect.TypeOf(leftKey))
+			}
+		}
+		fn := reflect.ValueOf(compareFunc)
+		comparator := func(a, b interface{}) int64 {
+			outs := fn.Call([]reflect.Value{
+				reflect.ValueOf(a),
+				reflect.ValueOf(b),
+			})
+			return outs[0].Int()
+		}
+
+		var leftValues []interface{}
+		for leftHasValue {
+			leftKey, leftValue, leftValues, leftHasValue = getSameKeyValues(leftChan, comparator, leftKey, leftValue)
+
+			// cartician join
+			if leftHasValue {
+				for _, a := range leftValues {
+					for _, b := range leftValues {
+						if a != nil && b != nil {
+							send(outChan, leftKey, a, b)
+						}
+					}
+				}
+			}
+		}
+
+	}
+	return ret
 }
