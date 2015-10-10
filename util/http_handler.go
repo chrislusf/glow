6df0005d903645984d1f135c446c384e8d40c60f
chrislusf <chris.lu@gmail.com>
2015-10-10 10:53:55 -0700

    check http write errors

--- util/http_handler.go
+++ util/http_handler.go
@@ -3,6 +3,7 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -20,6 +21,7 @@ func Json(w http.ResponseWriter, r *http.Request, httpStatus int, obj interface{
 		bytes, err = json.Marshal(obj)
 	}
 	if err != nil {
+		log.Printf("Marshal Error %v : %v", err, obj)
 		return
 	}
 	callback := r.FormValue("callback")
@@ -27,17 +29,24 @@ func Json(w http.ResponseWriter, r *http.Request, httpStatus int, obj interface{
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(httpStatus)
 		_, err = w.Write(bytes)
+		if err != nil {
+			log.Printf("Write Error %v : %v", err, string(bytes))
+			return
+		}
 	} else {
 		w.Header().Set("Content-Type", "application/javascript")
 		w.WriteHeader(httpStatus)
 		if _, err = w.Write([]uint8(callback)); err != nil {
+			log.Printf("Write1 Error %v", err)
 			return
 		}
 		if _, err = w.Write([]uint8("(")); err != nil {
+			log.Printf("Write2 Error %v", err)
 			return
 		}
 		fmt.Fprint(w, string(bytes))
 		if _, err = w.Write([]uint8(")")); err != nil {
+			log.Printf("Write3 Error %v", err)
 			return
 		}
 	}
