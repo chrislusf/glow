897730f3f154521e98a7624c1eea8787b64db18c
chrislusf <chris.lu@gmail.com>
2015-11-03 22:27:55 -0800

    clean locking

--- agent/named_datasore.go
+++ agent/named_datasore.go
@@ -9,6 +9,7 @@ import (
 )
 
 type ManagedDatasetShards struct {
+	sync.Mutex
 	dir            string
 	port           int
 	name2Store     map[string]store.DataStore
@@ -16,13 +17,13 @@ type ManagedDatasetShards struct {
 }
 
 func NewManagedDatasetShards(dir string, port int) *ManagedDatasetShards {
-	var lock sync.Mutex
-	return &ManagedDatasetShards{
-		dir:            dir,
-		port:           port,
-		name2Store:     make(map[string]store.DataStore),
-		name2StoreCond: sync.NewCond(&lock),
+	m := &ManagedDatasetShards{
+		dir:        dir,
+		port:       port,
+		name2Store: make(map[string]store.DataStore),
 	}
+	m.name2StoreCond = sync.NewCond(m)
+	return m
 }
 
 func (m *ManagedDatasetShards) doDelete(name string) {
@@ -39,8 +40,8 @@ func (m *ManagedDatasetShards) doDelete(name string) {
 
 func (m *ManagedDatasetShards) DeleteNamedDatasetShard(name string) {
 
-	m.name2StoreCond.L.Lock()
-	defer m.name2StoreCond.L.Unlock()
+	m.Lock()
+	defer m.Unlock()
 
 	m.doDelete(name)
 
@@ -48,7 +49,9 @@ func (m *ManagedDatasetShards) DeleteNamedDatasetShard(name string) {
 
 func (m *ManagedDatasetShards) CreateNamedDatasetShard(name string) store.DataStore {
 
-	m.name2StoreCond.L.Lock()
+	m.Lock()
+	defer m.Unlock()
+
 	_, ok := m.name2Store[name]
 	if ok {
 		m.doDelete(name)
@@ -57,7 +60,6 @@ func (m *ManagedDatasetShards) CreateNamedDatasetShard(name string) store.DataSt
 	s, err := store.NewLocalFileDataStore(m.dir, fmt.Sprintf("%s-%d", name, m.port))
 	if err != nil {
 		log.Printf("Failed to create a queue on disk: %v", err)
-		m.name2StoreCond.L.Unlock()
 		return nil
 	}
 
@@ -65,28 +67,23 @@ func (m *ManagedDatasetShards) CreateNamedDatasetShard(name string) store.DataSt
 	// println(name, "is broadcasting...")
 	m.name2StoreCond.Broadcast()
 
-	m.name2StoreCond.L.Unlock()
-
 	return s
 
 }
 
 func (m *ManagedDatasetShards) WaitForNamedDatasetShard(name string) store.DataStore {
 
-	var ds store.DataStore
-	var ok bool
+	m.Lock()
+	defer m.Unlock()
 
-	m.name2StoreCond.L.Lock()
 	for {
-		ds, ok = m.name2Store[name]
-		if ok {
-			break
+		if ds, ok := m.name2Store[name]; ok {
+			return ds
 		}
-		println(name, "is waiting to read...")
+		// println(name, "is waiting to read...")
 		m.name2StoreCond.Wait()
 	}
-	m.name2StoreCond.L.Unlock()
 
-	return ds
+	return nil
 
 }
