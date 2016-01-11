719e356ea666d68925e3567d770ee16e1699fe20
chrislusf <chris.lu@gmail.com>
2016-01-10 23:54:54 -0800

    remember remote executor's statuses

--- driver/scheduler/scheduler.go
+++ driver/scheduler/scheduler.go
@@ -2,15 +2,30 @@
 package scheduler
 
 import (
+	"sync"
+	"time"
+
 	"github.com/chrislusf/glow/driver/scheduler/market"
 )
 
 type Scheduler struct {
-	Leader       string
-	EventChan    chan interface{}
-	Market       *market.Market
-	option       *SchedulerOption
-	shardLocator *DatasetShardLocator
+	sync.Mutex
+
+	Leader                 string
+	EventChan              chan interface{}
+	Market                 *market.Market
+	option                 *SchedulerOption
+	shardLocator           *DatasetShardLocator
+	RemoteExecutorStatuses map[int32]*RemoteExecutorStatus
+}
+
+type RemoteExecutorStatus struct {
+	RequestTime  time.Time
+	InputLength  int
+	OutputLength int
+	ReadyTime    time.Time
+	RunTime      time.Time
+	StopTime     time.Time
 }
 
 type SchedulerOption struct {
@@ -25,12 +40,26 @@ type SchedulerOption struct {
 
 func NewScheduler(leader string, option *SchedulerOption) *Scheduler {
 	s := &Scheduler{
-		Leader:       leader,
-		EventChan:    make(chan interface{}),
-		Market:       market.NewMarket(),
-		shardLocator: NewDatasetShardLocator(option.ExecutableFileHash),
-		option:       option,
+		Leader:                 leader,
+		EventChan:              make(chan interface{}),
+		Market:                 market.NewMarket(),
+		shardLocator:           NewDatasetShardLocator(option.ExecutableFileHash),
+		option:                 option,
+		RemoteExecutorStatuses: make(map[int32]*RemoteExecutorStatus),
 	}
 	s.Market.SetScoreFunction(s.Score).SetFetchFunction(s.Fetch)
 	return s
+}
+
+func (s *Scheduler) getRemoteExecutorStatus(id int32) *RemoteExecutorStatus {
+	s.Lock()
+	defer s.Unlock()
+
+	status, ok := s.RemoteExecutorStatuses[id]
+	if ok {
+		return status
+	}
+	status = &RemoteExecutorStatus{}
+	s.RemoteExecutorStatuses[id] = status
+	return status
 }
