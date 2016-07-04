8e25a76f9bbeec48258a29f1c745930023fcc3bf
Yaxiong Zhao <justicezyx@gmail.com>
2016-07-03 21:34:01 -0700

    Add test for channel_util.go.

---
+++ util/channel_util_test.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+)
+
+func doubleIt(v reflect.Value) reflect.Value {
+	return reflect.ValueOf(v.Int() * 2)
+}
+
+func TestMergeChannel(t *testing.T) {
+	chan1 := make(chan reflect.Value)
+	chan2 := make(chan reflect.Value)
+
+	go func() {
+		chan1 <- reflect.ValueOf(1)
+		close(chan1)
+	}()
+
+	go func() {
+		chan2 <- reflect.ValueOf(2)
+		close(chan2)
+	}()
+
+	outChan := make(chan reflect.Value, 2)
+	MergeChannelTo([]chan reflect.Value{chan1, chan2}, doubleIt, outChan)
+
+	got := make([]int64, 0, 2)
+	for v := range outChan {
+		got = append(got, v.Int())
+	}
+	want := []int64{2, 4}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v want %v", got, want)
+	}
+}
