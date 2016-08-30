635c5002b71f87d24c4693c76bf32bf72cc254e1
Yaxiong <justicezyx@users.noreply.github.com>
2016-08-29 22:25:57 -0700

    Add collectOutput() for testing. (#71)

---
+++ flow/dataset_output_test.go
@@ -0,0 +1,21 @@
+package flow
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCollectOutput(t *testing.T) {
+	fCtx := New()
+	dataset := fCtx.Source(func(output chan string) {
+		output <- "a"
+		output <- "b"
+		output <- "c"
+	}, 1)
+
+	got := collectOutput(dataset, reflect.TypeOf(""))
+
+	if want := []string{"a", "b", "c"}; !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v want %v", got, want)
+	}
+}
